7614483a456e96d48e5441d5a69aca814f853965
Connor Harrison <connorh.uottawa@gmail.com>
2024-03-17 13:50:04 -0400

    perf: zod validator for employees

---
+++ src/lib/validators/employeeValidators.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { formatSIN } from "../utils";
+
+const EmployeeValidate = z.object({
+  full_name: z
+    .string()
+    .min(1, { message: "Full name can not be empty!" })
+    .max(32, { message: "Full name can not be longer than 32 characters!" }),
+  address: z.string().min(1, { message: "Address can not be empty!" }),
+  role: z.enum(["manager", "regular"]),
+  hotel_slug: z.string(),
+  sin: z.string().superRefine((data, ctx) => {
+    if (formatSIN(data).length != 11 || formatSIN(data) === null) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Enter a valid SIN!",
+        path: ["sin"],
+      });
+    }
+  }),
+});
+
+export default EmployeeValidate;
+
+export type TEmployeeValidate = z.infer<typeof EmployeeValidate>;
