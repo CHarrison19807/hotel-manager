48d460e852c328507dc47446c16c764c91f63d5c
Connor Harrison <connorh.uottawa@gmail.com>
2024-03-19 16:15:04 -0400

    refactor: add sin to customer validation

--- src/lib/validators/customerValidator.ts
+++ src/lib/validators/customerValidator.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
+import { formatSIN } from "../utils";
 
-const CustomerValidator = z.object({
+export const CustomerValidator = z.object({
   full_name: z
     .string()
     .min(1, { message: "Full name can not be empty!" })
@@ -9,6 +10,14 @@ const CustomerValidator = z.object({
     .string()
     .min(1, { message: "Address can not be empty!" })
     .max(32, { message: "Address can not be longer than 32 characters!" }),
+  sin: z.string().superRefine((data, ctx) => {
+    if (formatSIN(data).length != 11 || formatSIN(data) === null) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Enter a valid SIN!",
+      });
+    }
+  }),
 });
 
 export type TCustomerValidator = z.infer<typeof CustomerValidator>;
