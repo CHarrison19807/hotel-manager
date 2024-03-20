4706a2c533647f4a1b29aaba108f9c4409f6fb89
Connor Harrison <connorh.uottawa@gmail.com>
2024-03-20 15:11:31 -0400

    refactor: change validator for bookings

--- src/lib/validators/bookingValidator.ts
+++ src/lib/validators/bookingValidator.ts
@@ -1,18 +1,39 @@
 import { z } from "zod";
 
-const BookingValidator = z
+export const EmployeeBookingValidator = z
+  .object({
+    customer_sin: z.string().min(1, "Select a customer!"),
+    hotel_slug: z.string().min(1, "Select a hotel!"),
+    room_number: z.coerce
+      .number({ invalid_type_error: "Enter a valid room number!" })
+      .int("Enter a valid room number!")
+      .min(1, "Enter a valid room number!"),
+    check_in_date: z.date(),
+    check_out_date: z.date(),
+  })
+  .refine((data) => {
+    if (data.check_in_date > data.check_out_date) {
+      throw new Error("Check in date must be before check out date!");
+    }
+    return true;
+  });
+
+export const CustomerBookingValidator = z
   .object({
     check_in_date: z.date(),
     check_out_date: z.date(),
   })
-  .superRefine((data, ctx) => {
+  .refine((data) => {
     if (data.check_in_date > data.check_out_date) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: "Check-in date must be before check-out date!",
-        path: ["check_in_date", "check_out_date"],
-      });
+      throw new Error("Check in date must be before check out date!");
     }
+    return true;
   });
 
-export type TBookingValidator = z.infer<typeof BookingValidator>;
+export type TEmployeeBookingValidator = z.infer<
+  typeof EmployeeBookingValidator
+>;
+
+export type TCustomerBookingValidator = z.infer<
+  typeof CustomerBookingValidator
+>;
