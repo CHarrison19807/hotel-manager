eee71e9c24f52675e43b6c20d81649b953e1319d
Connor Harrison <connorh.uottawa@gmail.com>
2024-03-24 15:28:56 -0400

    style: create Status component

---
+++ src/components/Status.tsx
@@ -0,0 +1,19 @@
+interface StatusProps {
+  goodText: string;
+  badText: string;
+  status: boolean;
+  desired: boolean;
+}
+const Status = ({ goodText, badText, status, desired }: StatusProps) => {
+  const goodStyle =
+    "inline-flex items-center justify-center rounded-md font-medium bg-green-600 text-primary-foreground h-8 px-4 py-2 w-auto";
+  const badStyle =
+    "inline-flex items-center justify-center rounded-md font-medium bg-destructive text-primary-foreground h-8 px-4 py-2 w-auto";
+  console.log(status, desired);
+  if (status === desired) {
+    return <span className={goodStyle}>{goodText}</span>;
+  }
+  return <span className={badStyle}>{badText}</span>;
+};
+
+export default Status;
