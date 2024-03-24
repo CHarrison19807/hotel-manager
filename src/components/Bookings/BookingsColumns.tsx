ad53ae3242d8f5fbdc35b4bcbb76207f37e58bbe
Connor Harrison <connorh.uottawa@gmail.com>
2024-03-24 18:51:03 -0400

    feat: actions column for bookings

--- src/components/Bookings/BookingsColumns.tsx
+++ src/components/Bookings/BookingsColumns.tsx
@@ -1,9 +1,19 @@
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
-import { Booking } from "@/lib/booking";
+import { Booking, deleteBooking } from "@/lib/booking";
 import { formatPrice, unslugify } from "@/lib/utils";
 import { Button } from "../ui/button";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Link from "next/link";
+import { toast } from "sonner";
 
 const BookingColumns: ColumnDef<Booking>[] = [
   {
@@ -94,6 +104,53 @@ const BookingColumns: ColumnDef<Booking>[] = [
       const original: string = row.getValue("total_cost");
       const final: string = formatPrice(original);
       return final;
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ table, row }) => {
+      const booking: Booking = row.original;
+      const { booking_id, customer_sin } = booking;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(booking_id as string)
+              }
+            >
+              Copy booking ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link
+                onClick={async () => {
+                  const result = await deleteBooking(booking_id as string);
+                  if (result) {
+                    toast.error(
+                      "Unexpected error occurred while cancelling booking. Please try again."
+                    );
+                  } else {
+                    toast.success("Booking cancelled successfully!");
+                  }
+                }}
+                href={`/user/${customer_sin}/bookings/`}
+              >
+                Cancel booking
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
